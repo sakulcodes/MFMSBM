8af842446e378cf0e64fc5bb7a925a359ae210c9
sakulcodes <smahat@tamu.edu>
2022-11-15 14:22:08 -0600

    Skeleton for the logmargs function

---
+++ R/logmargs.R
@@ -0,0 +1,14 @@
+#function for getting m(Aj)
+logmargs <- function(clusterassign,data,J,beta.a,beta.b) #here J means Jth observation
+{
+  ################################################################
+
+  ## Input: clusterassign = clustering configuration, a n by 1 vector ##
+  ##        data = the adjacency matrix, a n by n matrix ##
+  ##        J = observation index ##
+  ##        n = number of observations ##
+  ##        beta.a, beta.b = hyperparameters for the prior on elements in Q matrix in Beta distribution ##
+
+  ## Output: m(A_j) in Algotithm 1 (collapsed sampler for MFM-SBM) ##
+
+}
