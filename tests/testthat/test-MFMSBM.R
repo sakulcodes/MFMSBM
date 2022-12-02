6eab2ecf0b3dd4ac751f307944cbdac69a888fec
sakulcodes <smahat@tamu.edu>
2022-12-02 13:16:45 -0600

    creating testthat skeleton for testing my code.

---
+++ tests/testthat/test-MFMSBM.R
@@ -0,0 +1,15 @@
+test_that("logmargs function works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("loglike function works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("CDMFM_new function works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("getDahl function works", {
+  expect_equal(2 * 2, 4)
+})
