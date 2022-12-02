f8b4ed0e230b27550663bdf8aaf9dba18682f9a3
sakulcodes <smahat@tamu.edu>
2022-12-02 15:51:49 -0600

    Updating the tests for CDMFM_new and getDahl function.

--- tests/testthat/test-MFMSBM.R
+++ tests/testthat/test-MFMSBM.R
@@ -1,19 +1,19 @@
 #######################################Testing_logmargs_function#######################################
 
 test_that("logmargs function works", {
-  cluster
   expect_equal(logmargs(c(2,2,3),matrix(data =1:9,nrow=3, ncol = 3),1,2,3), Inf)
   expect_equal(logmargs(c(2,2,4),matrix(data =1:9,nrow=3, ncol = 3),1,2,3), Inf)
-  expect_equal(logmargs(c(2,2,4),matrix(data =1:9,nrow=3, ncol = 3),1,2,3), NaN)
+  expect_equal(logmargs(c(2,2,4),matrix(data =1:9,nrow=3, ncol = 3),1,2,3), Inf)
   expect_equal(logmargs(c(8,8,8),matrix(data =1:9,nrow=3, ncol = 3),3,4,4), 0)
 })
+
 
 #######################################Testing_loglike_function#######################################
 
 test_that("loglike function works", {
-  expect_equal(loglike(c(2,3,4),matrix(data=1:25,nrow=5,ncol=5),matrix(data=1:9,nrow=3,ncol=3),1,1), NaN)
+  expect_warning(loglike(c(2,3,4),matrix(data=1:25,nrow=5,ncol=5),matrix(data=1:9,nrow=3,ncol=3),1,1), "NaNs produced")
   expect_equal(loglike(c(1,1,1),matrix(data=1:25,nrow=5,ncol=5),matrix(data=1:9,nrow=3,ncol=3),2,2), Inf)
-  expect_equal(loglike(c(5,5,5),matrix(data=1:25,nrow=5,ncol=5),matrix(data=1:9,nrow=3,ncol=3),2,3), NaN)
+  expect_warning(loglike(c(5,5,5),matrix(data=1:25,nrow=5,ncol=5),matrix(data=1:9,nrow=3,ncol=3),2,3), "NaNs produced")
 })
 
 #######################################Testing_CDMFM_new_function#######################################
@@ -21,9 +21,9 @@ test_that("loglike function works", {
 n <- 10
 A <- matrix(0,n,n)
 AAA <- matrix(0,n,n)
-
 test_that("CDMFM_new function works", {
-  #expect_equal(CDMFM_new(data = A, data1 = AAA, niterations = 1, beta.a = 1, beta.b = 1, GAMMA=1, LAMBDA = 1, initNClusters = 1)$zout, c(1,1,1,1,1,1,1,1,1))
+  expect_equal(CDMFM_new(data = A, data1 = AAA, niterations = 1, beta.a = 1, beta.b = 1, GAMMA=1, LAMBDA = 1, initNClusters = 1)$zout, NULL)
+  expect_equal(CDMFM_new(data = A, data1 = AAA, niterations = 1, beta.a = 1, beta.b = 1, GAMMA=1, LAMBDA = 1, initNClusters = 1)$Qout, NULL)
 })
 
 #######################################getDahl_function#######################################
@@ -33,6 +33,6 @@ AAA <- matrix(0,n,n)
 fit1 <- CDMFM_new(data = A, data1 = AAA, niterations = 2, beta.a = 1, beta.b = 1, GAMMA=1, LAMBDA = 1, initNClusters = 1)
 
 test_that("getDahl function works", {
-  #expect_equal(getDahl(fit1,1)$zout, c(1,1,1,1,1,1,1,1,1))
-  #expect_equal(getDahl(fit1,1)$Qout, matrix(data=0.02431389,nrow = 1,ncol=1))
+  expect_error(getDahl(fit1,100)$zout, "subscript out of bounds")
+  expect_error(getDahl(fit1,200)$zout, "subscript out of bounds")
 })
